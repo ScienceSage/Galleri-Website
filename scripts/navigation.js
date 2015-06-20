72136f331d4e405f354027d60647635672320bb3
Sage Thomas <sage.thomas@outlook.com>
2015-06-20 12:38:33 -0600

    navigation is now reusable

---
+++ scripts/navigation.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    var pages = "<p>Pages</p>";
+    pages += "<ul>";
+    pages += "<li> <a href='/index.html'>Home</a> </li>";
+    pages += "<li> <a href='/imageViewer.html'>Viewer</a> </li>";
+    pages += "</ul>";
+    
+    $( "#pages" ).html( pages );
+});
