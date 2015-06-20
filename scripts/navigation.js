aeccedcdd346c8c5948fc61005fd318696e327d0
Sage Thomas <sage.thomas@outlook.com>
2015-06-20 17:31:46 -0600

    GALLERI --> GALLERi

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,10 +1,3 @@
 $(document).ready(function(){
-    var pages = "<p>Pages</p>";
-    pages += "<ul>";
-    pages += "<li> <a href='/index.html'>Home</a> </li>";
-    pages += "<li> <a href='/imageViewer.html'>Viewer</a> </li>";
-    pages += "<li> <a href='/scripts/test.php'>Test</a> </li>";
-    pages += "</ul>";
     
-    $( "#pages" ).html( pages );
 });
