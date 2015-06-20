27a6d1fb630c16a2c1c516a613479279928408f3
Sage Thomas <sage.thomas@outlook.com>
2015-06-20 15:58:55 -0600

    mas php

---
+++ scripts/navigation.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<p>Pages</p>
+<ul>
+    <li> <a href='/index.html'>Home</a> </li>
+    <li> <a href='/imageViewer.html'>Viewer</a> </li>
+    <li> <a href='/scripts/test.php'>Test</a> </li>
+</ul> 
+
+</body>
+</html>
