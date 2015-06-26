e29dd30160449c6129d0a903b518f2acb10f7063
Sage Thomas <sage.thomas@outlook.com>
2015-06-26 14:30:35 -0400

    http

--- blob_test.php
+++ blob_test.php
@@ -1,5 +1,5 @@
 <?php
-echo "5 hello ";
+echo "6 hello ";
 require_once 'WindowsAzure\WindowsAzure.php';
 
 use WindowsAzure\Common\ServicesBuilder;
@@ -7,7 +7,7 @@ use WindowsAzure\Blob\Models\CreateContainerOptions;
 use WindowsAzure\Blob\Models\PublicAccessType;
 use WindowsAzure\Common\ServiceException;
 
-$storageConnString="DefaultEndpointsProtocol=https;AccountName=galleri;AccountKey=iMiW1aThQIgJvOEesVg/kxJYw2lit9LU56HKhvPNbzmI5mKv5SvKkI9QRtOVnPXLU3K4qYsMane8hyIoJ/qYvw==";
+$storageConnString="DefaultEndpointsProtocol=http;AccountName=galleri;AccountKey=iMiW1aThQIgJvOEesVg/kxJYw2lit9LU56HKhvPNbzmI5mKv5SvKkI9QRtOVnPXLU3K4qYsMane8hyIoJ/qYvw==";
 
 try 
 {
