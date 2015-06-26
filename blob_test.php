8e11db11802ed79643fc935b077ee98742aeaa48
Sage Thomas <sage.thomas@outlook.com>
2015-06-26 14:19:47 -0400

    make proxy

--- blob_test.php
+++ blob_test.php
@@ -1,5 +1,5 @@
 <?php
-echo "hello";
+echo "1 hello ";
 require_once 'WindowsAzure\WindowsAzure.php';
 
 use WindowsAzure\Common\ServicesBuilder;
@@ -7,11 +7,11 @@ use WindowsAzure\Blob\Models\CreateContainerOptions;
 use WindowsAzure\Blob\Models\PublicAccessType;
 use WindowsAzure\Common\ServiceException;
 
-//$connectionString=https;AccountName=galleri;AccountKey=iMiW1aThQIgJvOEesVg/kxJYw2lit9LU56HKhvPNbzmI5mKv5SvKkI9QRtOVnPXLU3K4qYsMane8hyIoJ/qYvw==;
-//
-//// Create blob REST proxy.
-//$blobRestProxy = ServicesBuilder::getInstance()->createBlobService($connectionString);
-//
+$connectionString=https;AccountName=galleri;AccountKey=iMiW1aThQIgJvOEesVg/kxJYw2lit9LU56HKhvPNbzmI5mKv5SvKkI9QRtOVnPXLU3K4qYsMane8hyIoJ/qYvw==;
+
+// Create blob REST proxy.
+$blobRestProxy = ServicesBuilder::getInstance()->createBlobService($connectionString);
+
 //// OPTIONAL: Set public access policy and metadata.
 //// Create container options object.
 //$createContainerOptions = new CreateContainerOptions(); 
