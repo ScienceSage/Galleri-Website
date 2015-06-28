cd7a0bd621b0cb23b6d7a3b1928565eaec25aa11
Sage Thomas <sage.thomas@outlook.com>
2015-06-28 17:33:56 -0400

    test

--- browse.php
+++ browse.php
@@ -22,7 +22,7 @@
     
     if(($result = sqlsrv_query($conn,"SELECT * FROM TestImages")) !== false){
         while( $row = sqlsrv_fetch_array( $result, SQLSRV_FETCH_ASSOC) ) {
-            echo $row['Name']."<br />"."Image: <img src=\"".$row['Image']."\"></img>";
+            echo $row['Name']."<br />"."<img src=\"browse.php?image=".$row['image']."\">";
         }
     }
     ?>
