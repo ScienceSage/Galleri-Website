8cbc255f8f5922d726317d81942988bc835197ad
Sage Thomas <sage.thomas@outlook.com>
2015-06-28 17:10:24 -0400

    added a browse page and deleted unused stuff

---
+++ browse.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>GALLERi Browser</title>
+    
+    <link href="styles/galleri.css"     		 rel="stylesheet" type="text/css">
+    
+    <script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
+    <script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+    <script src="/scripts/navigation.js"></script>
+</head>
+
+<body class="g-font">
+
+    <h1>Welcome to GALLERi Browser</h1>
+    <h2>Browse some art</h2>
+    
+    <?php
+    include_once 'sqltasks.php';
+    $conn = connect();
+    
+    if(($result = sqlsrv_query($conn,"SELECT * FROM TestImages")) !== false){
+        while( $row = sqlsrv_fetch_array( $result, SQLSRV_FETCH_ASSOC) ) {
+            echo $row['ID'].", ".$row['Name']."<br />";
+        }
+    }
+    ?>
+    
+    
+</body>
+</html> 
--- scripts/navigation.php
+++ scripts/navigation.php
@@ -6,6 +6,7 @@
 <ul>
     <li> <a href="/index.php">Home</a> </li>
     <li> <a href="/upload_image_page.php">Upload</a> </li>
+    <li> <a href="/browse.php">Browse</a> </li>
 </ul> 
 
 </body>
