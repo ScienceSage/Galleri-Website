ba5900f1544e9cb1f7c1fc364e327abc8cdb9bf0
Sage Thomas <sage.thomas@outlook.com>
2015-06-29 20:17:47 -0600

    test

--- blob_test.php
+++ blob_test.php
@@ -4,7 +4,7 @@ require_once 'WindowsAzure\WindowsAzure.php';
 use WindowsAzure\Common\ServicesBuilder;
 use WindowsAzure\Common\ServiceException;
 
-$connectionString = 'DefaultEndpointsProtocol=http;AccountName=galleri;AccountKey=iMiW1aThQIgJvOEesVg/kxJYw2lit9LU56HKhvPNbzmI5mKv5SvKkI9QRtOVnPXLU3K4qYsMane8hyIoJ/qYvw==';
+$connectionString = 'DefaultEndpointsProtocol=https;AccountName=galleri;AccountKey=iMiW1aThQIgJvOEesVg/kxJYw2lit9LU56HKhvPNbzmI5mKv5SvKkI9QRtOVnPXLU3K4qYsMane8hyIoJ/qYvw==';
 echo "connecting... ";
 $blobRestProxy = ServicesBuilder::getInstance()->createBlobService($connectionString);
 echo "done";
